457675170bc676525a9e11dde92e15f97b30cf3f
Sanjay Bhatt <sanjay.bhatt0621@gmail.com>
2024-07-10 00:21:47 +0530

    HC-102 | Asset Added --- Home Page Intial UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click Me</Button>
+      <section className="container remove-scrollbar my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 ConnectDoctor
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="doctorImage"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
